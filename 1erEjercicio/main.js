0c93e23410172c61855d24d7d2d2309cdceb0d81
Diegudeveloper <diegudeveloper@gmail.com>
2021-11-07 19:55:05 +0100

    Colocacion de los elementos en la card izquierda como se debe

--- 1erEjercicio/main.js
+++ 1erEjercicio/main.js
@@ -1,11 +1,16 @@
 const btnAceptar = document.querySelector('.btn_aceptar');
 const input = document.querySelector('.input_leftside');
 const cardR = document.querySelector('#cardRight_Text');
-
+const mensajeEror = document.querySelector('.msnError')
 
 btnAceptar.addEventListener('click', acepta)
 
-function acepta () {
-    cardR.textContent = input.value
-}
 
+
+function acepta () {
+    if (input.value === '') {
+        mensajeEror.textContent = ('Ustede debe Escribir una informacion');
+    } else {
+        cardR.textContent = input.value
+    }    
+}
