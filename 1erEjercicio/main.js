9794401d29811755c744905c3cdc9820f9e1289d
Diegudeveloper <diegudeveloper@gmail.com>
2021-11-07 17:43:43 +0100

    Se hicieron estilos y se muestra en la pantalla derecha el texto escrito pero sin arreglo.

--- 1erEjercicio/main.js
+++ 1erEjercicio/main.js
@@ -1,4 +1,11 @@
 const btnAceptar = document.querySelector('.btn_aceptar');
-const input = document.querySelector('.input_leftside').value;
+const input = document.querySelector('.input_leftside');
+const cardR = document.querySelector('#cardRight_Text');
 
+
+btnAceptar.addEventListener('click', acepta)
+
+function acepta () {
+    cardR.textContent = input.value
+}
 
