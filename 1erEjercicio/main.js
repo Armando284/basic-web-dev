ea60d8e6b3964ccc2519c68f0a563b4da7cfa7ea
Diegudeveloper <diegudeveloper@gmail.com>
2021-11-09 15:53:26 +0100

    prueba de colocar el valor del input en una lista pero sin resultado positivo.

--- 1erEjercicio/main.js
+++ 1erEjercicio/main.js
@@ -1,16 +1,36 @@
 const btnAceptar = document.querySelector('.btn_aceptar');
+const btnBorra = document.querySelector('.btn_borra');
 const input = document.querySelector('.input_leftside');
 const cardR = document.querySelector('#cardRight_Text');
 const mensajeEror = document.querySelector('.msnError')
+const cardList = document.querySelector('.cardList')
+
+const MostrarLista = [];
 
 btnAceptar.addEventListener('click', acepta)
+btnBorra.addEventListener('click', borra)
 
 
 
 function acepta () {
     if (input.value === '') {
-        mensajeEror.textContent = ('Ustede debe Escribir una informacion');
+        mensajeEror.textContent = ('Usted debe Escribir una informacion');
     } else {
-        cardR.textContent = input.value
+        cardR.textContent = input.value;
+        /*const newText = document.createElement("li");
+        const cardR = document.createElement("li");
+        cardR.textContent = input.value;
+        const cardText = document.querySelector('.cardText');
+        cardText.appendChild(cardR);*/
     }    
+}
+
+/*function addNewElement (acepta) {
+    MostrarLista.push(acepta)
+}*/
+
+function borra () {
+    input.value = ""
+    cardR.textContent = ""
+    mensajeEror.textContent = ""
 }
